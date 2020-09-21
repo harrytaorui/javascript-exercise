d377823f7f51a6abc40b347f1a6186afd655715c
harrytaorui <rui.tao@thoughtworks.com>
2020-09-21 20:43:30 +0800

    [taorui]test 3 complete

--- 3-object/values/index.js
+++ 3-object/values/index.js
@@ -1,3 +1,7 @@
 export default function countTypesNumber(source) {
-  // TODO 6: 在这里写实现代码
+  let count = 0;
+  Object.values(source).forEach(value => {
+    count += +value;
+  });
+  return count;
 }
