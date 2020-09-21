44837ca03d9c3ece9fa7fbadc194a85e7da86ca3
harrytaorui <rui.tao@thoughtworks.com>
2020-09-21 22:46:11 +0800

    [taorui]test 5-2 complete

--- 5-async/2-promise/index.js
+++ 5-async/2-promise/index.js
@@ -3,8 +3,17 @@ function fetchData(url) {
     const xhr = new XMLHttpRequest();
     // <-- start
     // TODO 22: 通过Promise实现异步请求
-
-    // end -->
+    xhr.open('GET', url);
+    xhr.onload = function() {
+      if (xhr.readyState === xhr.DONE) {
+        if (xhr.status === 200) {
+          resolve(xhr.responseText);
+        } else {
+          reject(xhr.statusText);
+        }
+      }
+    };
+    xhr.send(null);
   });
 }
 
