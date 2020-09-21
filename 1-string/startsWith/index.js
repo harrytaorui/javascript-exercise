e35c1f74b26c386d8c4eeb9d8c135fd32af83078
harrytaorui <rui.tao@thoughtworks.com>
2020-09-21 20:04:27 +0800

    [taorui]test 1 complete

--- 1-string/startsWith/index.js
+++ 1-string/startsWith/index.js
@@ -1,3 +1,9 @@
 export default function collectCarNumberCount(collection) {
-  // TODO 7: 在这里写实现代码
+  let count = 0;
+  collection.forEach(function(car) {
+    if (car.startsWith('粤A')) {
+      count += 1;
+    }
+  });
+  return count;
 }
